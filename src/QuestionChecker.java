13f92a610eb35abac39efc02c7ce1d3bb5a7f3cc
Spooky <61633921+Spoookyyy@users.noreply.github.com>
2023-10-31 04:25:33 -0700

    upload project

---
+++ src/QuestionChecker.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class QuestionChecker {
+
+    public static void main(String[] args) {
+        try {
+            List<String> lines = Files.readAllLines(new File("src/question.txt").toPath());
+
+            Collections.shuffle(lines);
+
+            Scanner scanner = new Scanner(System.in);
+
+            for (String line : lines) {
+                String[] parts = line.split(":");
+                if (parts.length != 2) {
+                    System.out.println("Invalid format: " + line);
+                    continue;
+                }
+
+                String question = parts[0].trim();
+                String correctAnswer = parts[1].trim();
+
+                while (true) {
+                    System.out.println(question);
+                    System.out.print("Your answer: ");
+                    String userAnswer = scanner.nextLine().trim();
+
+                    if (userAnswer.equalsIgnoreCase(correctAnswer)) {
+                        System.out.println("Correct!\n");
+                        break;
+                    } else {
+                        System.out.println("Incorrect. Try again.\n");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading the file.");
+            e.printStackTrace();
+        }
+    }
+}
